95577f19bfbec80b1b169ea1fc04aaee2cbffd90
KAUNGPYAEPAING <88584127+KAUNGPYAEPAING@users.noreply.github.com>
2021-08-23 20:24:14 +0630

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function whosPlaying(names) {
+    var playername = ["Angela", "Kaung Pyae", "James"];
+    var randomplayer = playername[Math.floor(Math.random()*playername.length)];
+    return randomplayer + " is going to buy lunch today";
+}
